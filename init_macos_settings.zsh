2b2297ebc1ea0728d8fbaa07ac420a8fecbd4bf1
pianwan <pianwan@outlook.com>
2025-05-13 21:12:37 -0700

    Update zsh plugins

--- init_macos_settings.zsh
+++ init_macos_settings.zsh
@@ -21,3 +21,24 @@ curl -L https://iterm2.com/shell_integration/`basename $SHELL` -o ~/.iterm2_shel
 source ~/.iterm2_shell_integration.`basename $SHELL`
 # zsh starship theme
 curl -sS https://starship.rs/install.sh | sh
+
+# zsh plugins
+typeset -A plugins
+plugins=(
+  zsh-autosuggestions        https://github.com/zsh-users/zsh-autosuggestions
+  zsh-completions            https://github.com/zsh-users/zsh-completions
+  fast-syntax-highlighting   https://github.com/zdharma-continuum/fast-syntax-highlighting
+)
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+for plugin in "${(@k)plugins}"; do
+  PLUGIN_DIR="$ZSH_CUSTOM/plugins/$plugin"
+  REPO="${plugins[$plugin]}"
+
+  if [ ! -d "$PLUGIN_DIR" ]; then
+    git clone "$REPO" "$PLUGIN_DIR"
+  else
+    git -C "$PLUGIN_DIR" pull --quiet
+  fi
+done
